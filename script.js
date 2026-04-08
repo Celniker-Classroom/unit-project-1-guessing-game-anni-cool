4a3811bc1f986066f47e0a461bfb5b6f912172b3
anni-cool <geierstangera0837@my.sduhsd.net>
2026-04-08 21:15:06 +0000

    extra

--- script.js
+++ script.js
@@ -1,27 +1,97 @@
 // add javascript here
+//Game state
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+let totalGuesses= 0;
+let scores= 0;
+
 // player name
 let playerName = prompt("Enter your name:"); 
 
 let range = 0;
-function setDifficulty() {
-    if (document.getElementById("e").checked) {
-        range = 3;
-        alert("Hi "+ playerName + ", you have selected Easy mode. You will be guessing a number between 1 and 3.");
-        document.getElementById("msg").innerText = "Easy";
+//Play  
+document.getElementById("playBtn").addEventListener("click", function() {
+    let radios = document.getElementsByName("level");
+    let range = 3; 
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            range = parseInt(radios[i].value);
+        }
     }
-    if (document.getElementById("m").checked) {
-        range = 10;
-        alert("Hi "+ playerName + ", you have selected Medium mode. You will be guessing a number between 1 and 10.");
-        document.getElementById("msg").innerText = "Medium";
-    }
-    if (document.getElementById("h").checked) {
-        range = 100;
-        alert("Hi "+ playerName + ", you have selected Hard mode. You will be guessing a number between 1 and 100.");
-    }
+
+    //round setup 
+    answer= Math.floor(Math.random() * range) + 1;
+    
+    document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range + ". Good luck!";
+    document.getElementById("guessBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = false;
+
+    let levelRadios = document.getElementsByName("level");
+    for (let i=0; i < levelRadios.length; i++) {
+        levelRadios[i].addEventListener("change", function() {
+        document.getElementById("playBtn").disabled = true;
+        }); 
+    }   
+
+});
+  
+
+//Guess
+
+//guessing 
+document.getElementById("guessBtn").addEventListener("click", function() {
+    let input = document.getElementById("guess").value;
+let num = parseInt(input);
+
+if (isNaN(num)) {
+    document.getElementById("msg").textContent = "Please enter a valid number.";
+    return;
 }
 
-document.getElementById("playBtn").addEventListener("click", function() {
-    setDifficulty();
-    let randomNumber = Math.floor(Math.random() * range) + 1;
-    document.getElementById("msg").innerText = id;
+guessCount++; 
+let diff = Math.abs(num-answer);
+
+//correct 
+if (num === answer) {
+    document.getElementById("msg").textContent = "Congratulations " + playerName + "! You guessed the correct number in " + guessCount + " guesses.";
+updateScore(guessCount);
+resetBtn();
+} else if (diff <= 2) {
+    document.getElementById("msg").textContent = "Very close! Try again.";
+} else if (diff <= 5) {
+    document.getElementById("msg").textContent = "Close! Keep trying.";
+} else {
+    document.getElementById("msg").textContent = "Not even close. Try again.";
+}
 });
+
+function updateScore(score) {
+totalWins ++; 
+totalGuesses += score;
+
+document.getElementById("wins").textContent = "Total Wins:" + totalWins;
+document.getElementById("avgScore").textContent = "Average Score:" + (totalGuesses / totalWins).toFixed(1);
+}
+
+function resetBtn() {
+            document.getElementById("guessBtn").disabled= true 
+        document.getElementById("giveUpBtn").disabled= true;
+            document.getElementById("playBtn").disabled = false;
+
+            document.getElementById("guess").value = "";
+            guessCount = 0;
+        };
+
+        //update leader boar
+        scores.push(score);
+        scores.sort(function(a, b) { return a - b; });
+
+        let leaderboard = document.getElementById("leaderboard");
+        for (let i = 0; i < leaderboard.length; i++) {
+            if (i < scores.length) {
+                leaderboard[i].textContent = scores[i];
+            } else {
+                leaderboard[i].textContent = "--";
+            }
+        }
