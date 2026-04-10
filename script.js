9a1467d481f189d56290dabc57114ffe824ef09d
anni-cool <geierstangera0837@my.sduhsd.net>
2026-04-10 06:25:47 +0000

    baisc?

--- script.js
+++ script.js
@@ -1,97 +1,103 @@
 // add javascript here
-//Game state
 let answer = 0;
 let guessCount = 0;
-let totalWins = 0;
-let totalGuesses= 0;
-let scores= 0;
+const scores = [];
 
-// player name
-let playerName = prompt("Enter your name:"); 
+let enteredName= prompt("Enter your name:"); 
 
-let range = 0;
-//Play  
-document.getElementById("playBtn").addEventListener("click", function() {
-    let radios = document.getElementsByName("level");
-    let range = 3; 
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            range = parseInt(radios[i].value);
+if(enteredName){
+    enteredName = enteredName.toLowerCase();
+    enteredName = enteredName.charAt(0).toUpperCase() + enteredName.slice(1);
+ }
+
+document.getElementById("playBtn").addEventListener("click", play);
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
+
+document.getElementById("msg").textContent = "Welcome " + enteredName + "! Please select a difficulty level and click PLAY to start the game.";
+
+function play(){
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for(let i=0; i<levels.length; i++){
+        if(levels[i].checked){
+            range = parseInt(levels[i].value);
+        }
+        levels[i].disabled = true;
+    }
+    document.getElementById("msg").textContent = enteredName + ", guess a number from 1 to " + range;
+    answer = Math.floor(Math.random()*range) +1;
+    guessCount = 0;
+
+
+    guessBtn.disabled = false;
+    giveUpBtn.disabled = false;
+    playBtn.disabled = true;
+}
+
+
+function makeGuess(){
+    let guess = parseInt(document.getElementById("guess").value);
+    if(isNaN(guess)){
+        msg.textContent = "Please enter a valid number";
+        return;
+    }
+    guessCount++;
+    if(guess == answer){
+        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        updateScore(guessCount);
+        resetGame();
+    }
+    else if(guess < answer && Math.abs(answer - guess) <= 2){
+        msg.textContent = "Hot, but too low, try again.";
+    }
+    else if(guess > answer && Math.abs(answer - guess) <= 2){
+        msg.textContent = "Hot, but too high, try again.";
+    }
+    else if(guess < answer && Math.abs(answer - guess) <= 5){
+        msg.textContent = "Warm, but too low, try again.";
+    }
+    else if(guess > answer && Math.abs(answer - guess) <= 5){
+        msg.textContent = "Warm, but too high, try again.";
+    }
+    else if(guess < answer && Math.abs(answer - guess) > 5){
+        msg.textContent = "Cold and too low, try again.";
+    }
+    else if(guess > answer && Math.abs(answer - guess) > 5){
+        msg.textContent = "Cold and too high, try again.";
+    }
+    }
+   
+
+
+function updateScore(score){
+    scores.push(score);
+    wins.textContent = "Total wins: " + scores.length;
+    let sum = 0;
+    for(let i = 0; i < scores.length; i++){
+        sum += scores[i]; // sum = sum + scores[i]
+    }
+    avgScore.textContent = "Average Score: " + (sum/scores.length).toFixed(1);
+
+
+    scores.sort(function(a,b){return a-b;}); // sort score increasing
+
+
+    let lb = document.getElementsByName("leaderboard");
+    for(let i = 0; i < lb.length; i++){
+        if(i < scores.length){
+            lb[i].textContent = scores[i];
         }
     }
+}
+function resetGame(){
+    guess.value = "";
+    guessBtn.disabled = true;
+    giveUpBtn.disabled = true;
+    playBtn.disabled = false;
+    e.disabled = false;
+    m.disabled = false;
+    h.disabled = false;
 
-    //round setup 
-    answer= Math.floor(Math.random() * range) + 1;
-    
-    document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range + ". Good luck!";
-    document.getElementById("guessBtn").disabled = false;
-    document.getElementById("giveUpBtn").disabled = false;
 
-    let levelRadios = document.getElementsByName("level");
-    for (let i=0; i < levelRadios.length; i++) {
-        levelRadios[i].addEventListener("change", function() {
-        document.getElementById("playBtn").disabled = true;
-        }); 
-    }   
-
-});
-  
-
-//Guess
-
-//guessing 
-document.getElementById("guessBtn").addEventListener("click", function() {
-    let input = document.getElementById("guess").value;
-let num = parseInt(input);
-
-if (isNaN(num)) {
-    document.getElementById("msg").textContent = "Please enter a valid number.";
-    return;
 }
 
-guessCount++; 
-let diff = Math.abs(num-answer);
-
-//correct 
-if (num === answer) {
-    document.getElementById("msg").textContent = "Congratulations " + playerName + "! You guessed the correct number in " + guessCount + " guesses.";
-updateScore(guessCount);
-resetBtn();
-} else if (diff <= 2) {
-    document.getElementById("msg").textContent = "Very close! Try again.";
-} else if (diff <= 5) {
-    document.getElementById("msg").textContent = "Close! Keep trying.";
-} else {
-    document.getElementById("msg").textContent = "Not even close. Try again.";
-}
-});
-
-function updateScore(score) {
-totalWins ++; 
-totalGuesses += score;
-
-document.getElementById("wins").textContent = "Total Wins:" + totalWins;
-document.getElementById("avgScore").textContent = "Average Score:" + (totalGuesses / totalWins).toFixed(1);
-}
-
-function resetBtn() {
-            document.getElementById("guessBtn").disabled= true 
-        document.getElementById("giveUpBtn").disabled= true;
-            document.getElementById("playBtn").disabled = false;
-
-            document.getElementById("guess").value = "";
-            guessCount = 0;
-        };
-
-        //update leader boar
-        scores.push(score);
-        scores.sort(function(a, b) { return a - b; });
-
-        let leaderboard = document.getElementById("leaderboard");
-        for (let i = 0; i < leaderboard.length; i++) {
-            if (i < scores.length) {
-                leaderboard[i].textContent = scores[i];
-            } else {
-                leaderboard[i].textContent = "--";
-            }
-        }
